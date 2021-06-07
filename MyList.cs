632f2df6a632c03caa96fa78ac9c129aaec64c50
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-06-07 14:35:22 +0500

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19
+{
+	class MyList <T>
+
+	{
+		public void Add(T value)
+		{
+			Node  <T> asd = new Node <T> ();
+			asd.value = value;
+			if (firstElement == null)
+			{
+				firstElement = asd;
+			}
+			else 
+			{
+				
+			}
+
+			Node <T> last = firstElement;
+
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			last.next = asd;
+
+		}
+		Node <T> firstElement;
+		
+	}
+}
