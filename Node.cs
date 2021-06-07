f848bff3cc3b877a1156bdffd6c354f4b5b426a8
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-06-07 15:48:26 +0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19
+{
+	class Node<T> 
+	{   public T value;
+		public Node<T> next;
+		public Node<T> prev;
+	}
+}
